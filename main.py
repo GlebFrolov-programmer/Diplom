ee1d2863e192754e38040ba842763e90ac00774d
glebfrolov <frolovgv@lad24.ru>
2024-03-02 13:23:19 +0300

    changed parameters

--- main.py
+++ main.py
@@ -1,32 +1,18 @@
 import math
-
 from experiment import Experiment
-from ega import EGA
-from nektar import Nektar
 
-
-# test_ega = EGA(task=task,
-#                size_population=10,
-#                count_population=int(task.count_places**0.5),
-#                # count_population=3,
-#                selection="o",
-#                mutation_chance=0.05,
-#                count_switches_gen=2,
-#                )
-
-
-exp = Experiment(count_exp=3,
+nektar_size = 9
+exp = Experiment(count_exp=10,
 
                  random_data=True,
-                 nektar_size=8,
+                 nektar_size=nektar_size,
 
                  count_scout=50,
                  ambit=15,
                  depth_search=-1,
 
                  size_population=5,
-                 count_population=int(math.factorial(8)**0.5),
-                 # count_population=3,
+                 count_population=int(math.factorial(nektar_size)**0.5),
                  selection="o",
                  mutation_chance=0.05,
                  count_switches_gen=2,
